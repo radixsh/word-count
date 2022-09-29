d01e11df0beab4abf9ac574644febb6fbea0b933
Radish <53844660+radixsh@users.noreply.github.com>
2022-09-28 21:23:22 -0700

    Write commonCount()

--- count.js
+++ count.js
@@ -1,26 +1,54 @@
 var style = "uc"
 
+function useUc(arg) {
+    style = "uc" 
+}
+
+function useCommon(arg) {
+    style = "common"
+}
+
 function ucCount(text) {
-    words = text.split(/\W+/);
+    words = text.replace(/[\s+-]/, " ").split(" ")
+    return words.length;
+    /*
     var wc = 0
     for (let i = 0; i < words.length; i++) {
         // If the "word" is not an empty string (i.e., the paragraph ended with
         // a space)
         if (words[i])
+            console.log("Empty word")
             wc++
     }
     return wc
+    */
+}
+
+function commonCount(text) {
+    words = text.replace(/\s+/, " ").split(" ")
+    return words.length;
+    /*
+    var wc = 0
+    for (let i = 0; i < words.length; i++) {
+        // If the "word" is not an empty string (i.e., the paragraph ended with
+        // a space)
+        if (words[i])
+            console.log("Empty word")
+            wc++
+    }
+    return wc
+    */
 }
 
 // https://stackoverflow.com/questions/25976564/onkeyup-event-in-textarea
 function count() {
     var input = document.getElementById("input").value;
-    if (style == "uc") {
+    style = document.getElementById("style").value;
+    if (style == "uc")
         var wordcount = ucCount(input);
-    } else if (style == "common") {
+    else if (style == "common")
         var wordcount = commonCount(input);
-    }
    
     output = document.getElementById("output");
-    output.innerHTML = "Wordcount: " + wordcount;
+    output.innerHTML = wordcount;
 }
